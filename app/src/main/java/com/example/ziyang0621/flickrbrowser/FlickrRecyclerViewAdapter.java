6770003b3688a0143cef92628228e3056dc9302d
ziyang <ziyang0621@gmail.com>
2014-12-09 15:21:01 -0800

    project update

--- app/src/main/java/com/example/ziyang0621/flickrbrowser/FlickrRecyclerViewAdapter.java
+++ app/src/main/java/com/example/ziyang0621/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -44,4 +44,9 @@ public class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrImageV
     public int getItemCount() {
         return (null != mPhotoList ? mPhotoList.size() : 0);
     }
+
+    public void loadNewData(List<Photo> newPhotos) {
+        mPhotoList = newPhotos;
+        notifyDataSetChanged();
+    }
 }
--- app/src/main/java/com/example/ziyang0621/flickrbrowser/MainActivity.java
+++ app/src/main/java/com/example/ziyang0621/flickrbrowser/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.ziyang0621.flickrbrowser;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,11 +29,22 @@ public class MainActivity extends BaseActivity {
 
         activateToolbar();
 
-        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        ProcessPhotos processPhotos = new ProcessPhotos("android, lollipop", true);
-        processPhotos.execute();
+        mFlickrRecyclerViewAdapter = new FlickrRecyclerViewAdapter(MainActivity.this, new ArrayList<Photo>());
+        mRecyclerView.setAdapter(mFlickrRecyclerViewAdapter);
+        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, mRecyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this, "Normal tap", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+                Toast.makeText(MainActivity.this, "Long tap", Toast.LENGTH_LONG).show();
+            }
+        }));
     }
 
 
@@ -50,29 +66,48 @@ public class MainActivity extends BaseActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if (id == R.id.menu_search) {
+            Intent intent = new Intent(this, SearchActivity.class);
+            startActivity(intent);
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
-   public class ProcessPhotos extends GetFlickrJsonData {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String query = getSavedPreferenceData(FLICKR_QUERY);
+        if (query.length() > 0) {
+            ProcessPhotos processPhotos = new ProcessPhotos(query, true);
+            processPhotos.execute();
+        }
+    }
 
-       public ProcessPhotos(String searchCriteria, boolean matchAll) {
-           super(searchCriteria, matchAll);
-       }
+    private String getSavedPreferenceData(String key) {
+        SharedPreferences sharePref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        return sharePref.getString(key, "");
+    }
 
-       public void execute() {
-           super.execute();
-           ProcessData processData = new ProcessData();
-           processData.execute();
-       }
+    public class ProcessPhotos extends GetFlickrJsonData {
 
-       public class ProcessData extends DownloadJsonData {
+        public ProcessPhotos(String searchCriteria, boolean matchAll) {
+            super(searchCriteria, matchAll);
+        }
 
-           protected void onPostExecute(String webData) {
-               super.onPostExecute(webData);
-               mFlickrRecyclerViewAdapter = new FlickrRecyclerViewAdapter(MainActivity.this, getPhotos());
-               mRecyclerView.setAdapter(mFlickrRecyclerViewAdapter);
-           }
-       }
-   }
+        public void execute() {
+            super.execute();
+            ProcessData processData = new ProcessData();
+            processData.execute();
+        }
+
+        public class ProcessData extends DownloadJsonData {
+
+            protected void onPostExecute(String webData) {
+                super.onPostExecute(webData);
+                mFlickrRecyclerViewAdapter.loadNewData(getPhotos());
+            }
+        }
+    }
 }
